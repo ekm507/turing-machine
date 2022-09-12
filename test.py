6c910b7a2d77ffc07220b3d6c5e396b926d7dc73
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2022-09-12 13:52:25 +0430

    test a program running

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ program = """
 q0 x -> x, R q0
 q0 0 -> x, R q10
 q0 1 -> x, R q11
-q0 # -> #, R, q50
+q0 # -> #, R q50
 
 q10 1 -> 1, R q10
 q10 0 -> 0, R q10
@@ -14,11 +14,11 @@ q10 # -> #, R q20
 
 q20 x -> x, R q20
 q20 0 -> x, L q30
-q20 1 -> 1, L, q_reject
-q20 _ -> _, L, q_reject
+q20 1 -> 1, L q_reject
+q20 _ -> _, L q_reject
 
-q30 x -> x, L, q30
-q30 # -> #, L, q40
+q30 x -> x, L q30
+q30 # -> #, L q40
 
 q40 0 -> 0, L q40
 q40 1 -> 1, L q40
@@ -32,8 +32,27 @@ q50 1 -> 1, L q_reject
 
 """.replace('\n\n', '\n').strip('\n')
 
+tape_string = '0000#0000_'
+
 m1 = Turing()
 m1.set_program_by_string(program)
 
 # test if setting program works
-pprint(m1.program)
+
+m1.tape.set_tape(tape_string)
+# pprint(m1.program)
+
+maximum_steps = 1000
+for _ in range(maximum_steps):
+    print('-'*len(tape_string))
+    print(m1.state)
+    print(' '*m1.index+'o')
+    print(m1.tape.get_tape_string())
+    m1.one_step()
+    if m1.halt == True:
+        print()
+        print()
+        print('final state is', m1.state)
+        break
+else:
+    print('machine did not halt!')
