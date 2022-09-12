d0570bca76fafb1f3f1164770ad830a22978873a
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2022-09-12 13:21:16 +0430

    get program better

--- turing_machine.py
+++ turing_machine.py
@@ -9,7 +9,7 @@ class Turing():
     def set_program_by_string(self, program:str):
         transitions = program.split('\n')
         for transition in transitions:
-            parameters = re.findall(r'(.+) +(.+) *-> *(.+) *, *(.+) +(.+)', transition)[0]
+            parameters = re.findall(r'([^ ]+) +([^ ]+) *-> *([^ ]+) *, *([^ ]+) +([^ ]+)', transition)[0]
             init_state, read_string, overwrite_string, next_direction, final_state = parameters
 
             if init_state not in self.program:
